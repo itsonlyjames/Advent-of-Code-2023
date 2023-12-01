f4b7a69cb06ba0d1ec57f36c1fe25cd736431321
itsonlyjames <jamesalexandre1995@gmail.com>
2023-12-01 17:29:25 +1100

    Day 1 Completed

---
+++ 1/a.ts
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import path from 'path'
+const txt = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf-8')
+
+const lines = txt.split('\n')
+
+const array: any = []
+let pos = 0
+lines.forEach((line) => {
+    const split = line.split('')
+    if (!array[pos]) array[pos] = []
+    split.forEach((val) => {
+        const number = parseInt(val)
+        if (!isNaN(number)) {
+            array[pos].push(number)
+        }
+    })
+    pos++
+})
+
+let total = 0
+array.forEach((val: any) => {
+    const first = val[0]
+    const last = val[val.length - 1]
+    const combined = `${first}` + `${last}`
+    total += parseInt(combined)
+})
+
+console.log(total)
---
+++ 1/b.ts
@@ -0,0 +1,51 @@
+import fs from 'fs'
+import path from 'path'
+const txt = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf-8')
+
+const lines = txt.split('\n')
+
+const array: any = []
+let pos = 0
+const value: any = {
+    on: 1,
+    tw: 2,
+    thre: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eigh: 8,
+    nine: 9
+}
+
+lines.forEach((line, index) => {
+    if (!array[pos]) array[pos] = []
+
+    const match = [
+        ...line.matchAll(
+            /on(?=e)|tw(?=o)|thre|four|five|six|seven|eigh(?=t)|nine|1|2|3|4|5|6|7|8|9/g
+        )
+    ]
+    match.forEach((val) => {
+        const number = parseInt(val[0])
+        if (!isNaN(number)) {
+            array[pos].push(number)
+        } else {
+            array[pos].push(value[val[0]])
+        }
+    })
+
+    pos++
+})
+
+let total = 0
+array
+    .filter((n: any) => !!n)
+    .forEach((val: any) => {
+        const first = val[0]
+        const last = val.pop()
+        const combined = `${first}` + `${last}`
+        total += parseInt(combined)
+    })
+
+console.log(total)

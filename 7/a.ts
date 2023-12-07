9769d1b49266ccc767ed77ead1b018046dcf2923
itsonlyjames <jamesalexandre1995@gmail.com>
2023-12-07 18:26:15 +1100

    Day 7 Completed (Learnt lots)

--- 7/a.ts
+++ 7/a.ts
@@ -1,1 +1,38 @@
 const lines = Deno.readTextFileSync('./input.txt').split(/\n/)
+
+type Hand = { cards: string; bid: number; kinds: number[] }
+type CardCounts = Record<string, number>
+
+const cardRanks = 'AKQJT98765432'.split('')
+
+const total = lines
+    .map((line: string) => {
+        const [cards, bid] = line.split(' ')
+        const counts: CardCounts = {}
+        for (let i = 0; i < cards.length; i++) {
+            if (counts[cards[i]]) counts[cards[i]]++
+            else counts[cards[i]] = 1
+        }
+        const kinds = Object.values(counts)
+        kinds.sort((a, b) => b - a)
+        return { cards, kinds, bid: Number(bid) }
+    })
+    .toSorted((h1: Hand, h2: Hand) => {
+        for (let i = 0; i < 5; i++) {
+            if (!h1.kinds[i] || !h2.kinds[i]) break
+            if (h1.kinds[i] > h2.kinds[i]) return 1
+            if (h1.kinds[i] < h2.kinds[i]) return -1
+        }
+
+        for (let i = 0; i < 5; i++) {
+            if (cardRanks.indexOf(h1.cards[i]) < cardRanks.indexOf(h2.cards[i]))
+                return 1
+            if (cardRanks.indexOf(h1.cards[i]) > cardRanks.indexOf(h2.cards[i]))
+                return -1
+        }
+
+        return 0
+    })
+    .reduce((sum, { bid }, i) => sum + bid * (i + 1), 0)
+
+console.log('🚀', total)
--- 7/b.ts
+++ 7/b.ts
@@ -1,1 +1,45 @@
 const lines = Deno.readTextFileSync('./input.txt').split(/\n/)
+
+type Hand = { cards: string; bid: number; kinds: number[] }
+type CardCounts = Record<string, number>
+
+const cardRanks = 'AKQT98765432J'.split('')
+
+const total = lines
+    .map((line: string) => {
+        const [cards, bid] = line.split(' ')
+        const counts: CardCounts = {}
+        for (let i = 0; i < cards.length; i++) {
+            if (counts[cards[i]]) counts[cards[i]]++
+            else counts[cards[i]] = 1
+        }
+        const maxCard = Object.keys(counts)
+            .filter((c) => c !== 'J')
+            .sort((a, b) => counts[b] - counts[a])[0]
+        if (counts.J && maxCard) {
+            counts[maxCard] += counts.J
+            delete counts.J
+        }
+        const kinds = Object.values(counts)
+        kinds.sort((a, b) => b - a)
+        return { cards, kinds, bid: Number(bid) }
+    })
+    .toSorted((h1: Hand, h2: Hand) => {
+        for (let i = 0; i < 5; i++) {
+            if (!h1.kinds[i] || !h2.kinds[i]) break
+            if (h1.kinds[i] > h2.kinds[i]) return 1
+            if (h1.kinds[i] < h2.kinds[i]) return -1
+        }
+
+        for (let i = 0; i < 5; i++) {
+            if (cardRanks.indexOf(h1.cards[i]) < cardRanks.indexOf(h2.cards[i]))
+                return 1
+            if (cardRanks.indexOf(h1.cards[i]) > cardRanks.indexOf(h2.cards[i]))
+                return -1
+        }
+
+        return 0
+    })
+    .reduce((sum, { bid }, i) => sum + bid * (i + 1), 0)
+
+console.log('🚀', total)
